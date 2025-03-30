3ad9db94971762c38b381f689cf7549f3097cb81
M-Shourav <muhammadshourav625@gmail.com>
2025-03-30 14:10:52 +0600

    some changes

--- app/(client)/cart/page.tsx
+++ app/(client)/cart/page.tsx
@@ -1,198 +1,274 @@
-'use client'
-import { createCheckoutSession, MetaData } from '@/actions/createCheckoutSession'
-import Container from '@/components/Container'
-import EmptyCart from '@/components/EmptyCart'
-import Loading from '@/components/Loading'
-import NoAccessToCart from '@/components/NoAccessToCart'
-import PriceFormatter from '@/components/PriceFormatter'
-import PriceQuantity from '@/components/PriceQuantity'
-import { Button } from '@/components/ui/button'
-import { urlFor } from '@/sanity/lib/image'
-import userCartStore from '@/store'
-import { useAuth, useUser } from '@clerk/nextjs'
-import { ShoppingBag, Trash2 } from 'lucide-react'
-import Image from 'next/image'
-import Link from 'next/link'
-import React, { useEffect, useState } from 'react'
-import toast from 'react-hot-toast'
+"use client";
+import {
+  createCheckoutSession,
+  MetaData,
+} from "@/actions/createCheckoutSession";
+import Container from "@/components/Container";
+import EmptyCart from "@/components/EmptyCart";
+import Loading from "@/components/Loading";
+import NoAccessToCart from "@/components/NoAccessToCart";
+import PriceFormatter from "@/components/PriceFormatter";
+import PriceQuantity from "@/components/PriceQuantity";
+import { Button } from "@/components/ui/button";
+import { urlFor } from "@/sanity/lib/image";
+import userCartStore from "@/store";
+import { useAuth, useUser } from "@clerk/nextjs";
+import { ShoppingBag, Trash2 } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 const CartPage = () => {
-  const {getItemCount,getSubTotalPrice,getTotalPrice,resetCart,deleteCartProduct}=userCartStore()
-  const getGroupItem=userCartStore((state)=>state.getGroupItem())
-  const {isSignedIn}=useAuth()
-  const {user}=useUser()
-  const [isLoading,setIsLoading]=useState(false)
-  const [isClient,setIsClient]=useState(false)
-  useEffect(()=>{
-    setIsClient(true)
-  },[])
-  if(!isClient){
-    return <Loading/>
+  const {
+    getItemCount,
+    getSubTotalPrice,
+    getTotalPrice,
+    resetCart,
+    deleteCartProduct,
+  } = userCartStore();
+  const getGroupItem = userCartStore((state) => state.getGroupItem());
+  const { isSignedIn } = useAuth();
+  const { user } = useUser();
+  const [isLoading, setIsLoading] = useState(false);
+  const [isClient, setIsClient] = useState(false);
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+  if (!isClient) {
+    return <Loading />;
   }
-  const handleCheckOut=async()=>{
-    setIsLoading(true)
+  const handleCheckOut = async () => {
+    setIsLoading(true);
     try {
-      const metadata:MetaData={
-        orderNumber:crypto.randomUUID(),
-        customerName:user?.fullName ?? "unknown",
-        customerEmail:user?.emailAddresses[0]?.emailAddress ?? "unknown",
-        clerkUserId:user!?.id
+      const metadata: MetaData = {
+        orderNumber: crypto.randomUUID(),
+        customerName: user?.fullName ?? "unknown",
+        customerEmail: user?.emailAddresses[0]?.emailAddress ?? "unknown",
+        clerkUserId: user!.id,
+      };
+
+      const checkoutUrl = await createCheckoutSession(getGroupItem, metadata);
+      if (checkoutUrl) {
+        window.location.href = checkoutUrl;
       }
-      
-      const checkoutUrl=await createCheckoutSession(getGroupItem,metadata)
-      if(checkoutUrl){
-        window.location.href=checkoutUrl
-      }
-      
     } catch (error) {
-      console.error('Error creating checkout session:',error)
-    }finally{
-      setIsLoading(false)
+      console.error("Error creating checkout session:", error);
+    } finally {
+      setIsLoading(false);
     }
-  }
+  };
 
-  const handleProductDelete=(_id:string)=>{
-    deleteCartProduct(_id)
-    toast.success('product delete successfully')
-  }
+  const handleProductDelete = (_id: string) => {
+    deleteCartProduct(_id);
+    toast.success("product delete successfully");
+  };
 
-  const handleResetCart=()=>{
-    const confirmed=window.confirm("Are you sure to reset your cart?")
-    if(confirmed){
-      resetCart()
-      toast.success("Your cart is reset successfully")
+  const handleResetCart = () => {
+    const confirmed = window.confirm("Are you sure to reset your cart?");
+    if (confirmed) {
+      resetCart();
+      toast.success("Your cart is reset successfully");
     }
-  }
+  };
 
   return (
-    <div className='pb-10 '>
-     {isSignedIn ? (
-        <Container className='py-10 bg-gray-50'>
+    <div className="pb-10 ">
+      {isSignedIn ? (
+        <Container className="py-10 bg-gray-50">
           {getGroupItem.length ? (
-          <>
-          <div className='flex items-center gap-2 py-5'>
-            <ShoppingBag className='w-7 h-7 text-gray-800'/>
-            <h1 className='text-2xl font-semibold'>Shopping Cart</h1>
-          </div>
-          <div className='grid lg:grid-cols-3 md:gap-8'>
-            <div className='lg:col-span-1'>
-              <div className=' hidden md:inline-block w-full bg-white p-6 rounded-md
-               border'>
-              <h2 className='text-xl font-semibold mb-4'>Order summary</h2>
-              <div className='space-y-4 mb-2'>
-              <div className='flex items-center justify-between'>
-                <span className='text-lg font-semibold'>Subtotal</span>
-                <PriceFormatter amount={getSubTotalPrice()}/>
+            <>
+              <div className="flex items-center gap-2 py-5">
+                <ShoppingBag className="w-7 h-7 text-gray-800" />
+                <h1 className="text-2xl font-semibold">Shopping Cart</h1>
               </div>
-              </div>
-              <div className='space-y-4'>
-              <div className='flex items-center justify-between'>
-                <span className='text-lg font-semibold'>Discount</span>
-                <PriceFormatter amount={getSubTotalPrice() - getTotalPrice()}/>
-              </div>
-              </div>
-              <div className='w-full border border-gray-300 mt-1'/>
-              <div className='space-y-4 mt-2'>
-              <div className='flex items-center justify-between'>
-                <span className='text-lg font-semibold'>Total</span>
-                <PriceFormatter amount={getTotalPrice()}/>
-              </div>
-              </div>
-              <div className='space-y-4 py-5 flex flex-col items-center'>
-                <Button onClick={handleCheckOut} className='w-full cursor-pointer' disabled={isLoading}>
-                  {isLoading ? <div className='flex items-center justify-center gap-x-5 w-full text-base'>
-                    <p>Processing...</p>
-                    <span className='text-xl animate-spin'><AiOutlineLoading3Quarters/></span>
-                  </div> :"Proceed to checkout"}
-                </Button>
-                <Link href={'/'} className='text-base font-semibold hover:underline hover:text-blue-400 duration-300'>Continue Shopping</Link>
-              </div>
-              </div>
-            </div>
-            <div className='lg:col-span-2 pb-5'>
-              <div className='grid grid-cols-5 md:grid-cols-6 border p-2.5 rounded-tr-lg rounded-tl-lg
-              bg-white text-base font-normal'>
-                <b className='col-span-2 lg:col-span-3'>Product</b>
-                <b>Price</b>
-                <b>Quantity</b>
-                <b>Total</b>
-              </div>
-              <div>
-                {getGroupItem?.map((product)=>{
-                  const itemCount=getItemCount(product.product._id)
-                  return(
-                    <div key={product?.product._id} className='grid grid-cols-5 md:grid-cols-6 border-b p-2.5 last:border-b-0'>
-                     <div className='col-span-2 md:col-span-3 flex items-center gap-2'>
-                      <Trash2 className='cursor-pointer hover:text-red-600 duration-300 ease-in-out' onClick={()=>{
-                        handleProductDelete(product.product._id)
-                      }}/>
-                      {product.product?.image && <Image src={urlFor(product.product.image).url()} alt='products-image'
-                       width={300}
-                       height={300}
-                       className='w-14 h-14 object-cover'/>}
-                       <p className='text-sm hidden sm:inline-block'>{product.product?.name}</p>
-                     </div>
-                     <div className='flex items-center'>
-                      <PriceFormatter amount={product.product.price}/>
-                     </div>
-                     <PriceQuantity className='flex items-center gap-x-1 text-sm' product={product.product}/>
-                     <div className='flex items-center'>
-                      <PriceFormatter  amount={product.product.price ? product.product.price * itemCount :0}/>
-                     </div>
+              <div className="grid lg:grid-cols-3 md:gap-8">
+                <div className="lg:col-span-1">
+                  <div
+                    className=" hidden md:inline-block w-full bg-white p-6 rounded-md
+               border"
+                  >
+                    <h2 className="text-xl font-semibold mb-4">
+                      Order summary
+                    </h2>
+                    <div className="space-y-4 mb-2">
+                      <div className="flex items-center justify-between">
+                        <span className="text-lg font-semibold">Subtotal</span>
+                        <PriceFormatter amount={getSubTotalPrice()} />
+                      </div>
                     </div>
-                  )
-                })}
-                <div className='py-5'>
-                  <Button variant="destructive" className=' cursor-pointer' onClick={handleResetCart}>Reset Cart</Button>
+                    <div className="space-y-4">
+                      <div className="flex items-center justify-between">
+                        <span className="text-lg font-semibold">Discount</span>
+                        <PriceFormatter
+                          amount={getSubTotalPrice() - getTotalPrice()}
+                        />
+                      </div>
+                    </div>
+                    <div className="w-full border border-gray-300 mt-1" />
+                    <div className="space-y-4 mt-2">
+                      <div className="flex items-center justify-between">
+                        <span className="text-lg font-semibold">Total</span>
+                        <PriceFormatter amount={getTotalPrice()} />
+                      </div>
+                    </div>
+                    <div className="space-y-4 py-5 flex flex-col items-center">
+                      <Button
+                        onClick={handleCheckOut}
+                        className="w-full cursor-pointer"
+                        disabled={isLoading}
+                      >
+                        {isLoading ? (
+                          <div className="flex items-center justify-center gap-x-5 w-full text-base">
+                            <p>Processing...</p>
+                            <span className="text-xl animate-spin">
+                              <AiOutlineLoading3Quarters />
+                            </span>
+                          </div>
+                        ) : (
+                          "Proceed to checkout"
+                        )}
+                      </Button>
+                      <Link
+                        href={"/"}
+                        className="text-base font-semibold hover:underline hover:text-blue-400 duration-300"
+                      >
+                        Continue Shopping
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+                <div className="lg:col-span-2 pb-5">
+                  <div
+                    className="grid grid-cols-5 md:grid-cols-6 border p-2.5 rounded-tr-lg rounded-tl-lg
+              bg-white text-base font-normal"
+                  >
+                    <b className="col-span-2 lg:col-span-3">Product</b>
+                    <b>Price</b>
+                    <b>Quantity</b>
+                    <b>Total</b>
+                  </div>
+                  <div>
+                    {getGroupItem?.map((product) => {
+                      const itemCount = getItemCount(product.product._id);
+                      return (
+                        <div
+                          key={product?.product._id}
+                          className="grid grid-cols-5 md:grid-cols-6 border-b p-2.5 last:border-b-0"
+                        >
+                          <div className="col-span-2 md:col-span-3 flex items-center gap-2">
+                            <Trash2
+                              className="cursor-pointer hover:text-red-600 duration-300 ease-in-out"
+                              onClick={() => {
+                                handleProductDelete(product.product._id);
+                              }}
+                            />
+                            {product.product?.image && (
+                              <Image
+                                src={urlFor(product.product.image).url()}
+                                alt="products-image"
+                                width={300}
+                                height={300}
+                                className="w-14 h-14 object-cover"
+                              />
+                            )}
+                            <p className="text-sm hidden sm:inline-block">
+                              {product.product?.name}
+                            </p>
+                          </div>
+                          <div className="flex items-center">
+                            <PriceFormatter amount={product.product.price} />
+                          </div>
+                          <PriceQuantity
+                            className="flex items-center gap-x-1 text-sm"
+                            product={product.product}
+                          />
+                          <div className="flex items-center">
+                            <PriceFormatter
+                              amount={
+                                product.product.price
+                                  ? product.product.price * itemCount
+                                  : 0
+                              }
+                            />
+                          </div>
+                        </div>
+                      );
+                    })}
+                    <div className="py-5">
+                      <Button
+                        variant="destructive"
+                        className=" cursor-pointer"
+                        onClick={handleResetCart}
+                      >
+                        Reset Cart
+                      </Button>
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
-          </div>
-          {/* mobile */}
-        <div className='md:hidden w-full'>
-        <div className='p-4 rounded-lg border mx-4'>
-              <h2 className='text-xl font-semibold mb-4'>Order summary</h2>
-              <div className='space-y-4 mb-2'>
-              <div className='flex items-center justify-between'>
-                <span className='text-lg font-semibold'>Subtotal</span>
-                <PriceFormatter amount={getSubTotalPrice()}/>
+              {/* mobile */}
+              <div className="md:hidden w-full">
+                <div className="p-4 rounded-lg border mx-4">
+                  <h2 className="text-xl font-semibold mb-4">Order summary</h2>
+                  <div className="space-y-4 mb-2">
+                    <div className="flex items-center justify-between">
+                      <span className="text-lg font-semibold">Subtotal</span>
+                      <PriceFormatter amount={getSubTotalPrice()} />
+                    </div>
+                  </div>
+                  <div className="space-y-4">
+                    <div className="flex items-center justify-between">
+                      <span className="text-lg font-semibold">Discount</span>
+                      <PriceFormatter
+                        amount={getSubTotalPrice() - getTotalPrice()}
+                      />
+                    </div>
+                  </div>
+                  <div className="w-full border border-gray-300 mt-1" />
+                  <div className="space-y-4 mt-2">
+                    <div className="flex items-center justify-between">
+                      <span className="text-lg font-semibold">Total</span>
+                      <PriceFormatter amount={getTotalPrice()} />
+                    </div>
+                  </div>
+                  <div className="space-y-4 py-5 flex flex-col items-center">
+                    <Button
+                      onClick={handleCheckOut}
+                      className="w-full cursor-pointer"
+                      disabled={isLoading}
+                    >
+                      {isLoading ? (
+                        <div className="flex items-center justify-center gap-x-5 w-full text-base">
+                          <p>Processing...</p>
+                          <span className="text-xl animate-spin">
+                            <AiOutlineLoading3Quarters />
+                          </span>
+                        </div>
+                      ) : (
+                        "Proceed to checkout"
+                      )}
+                    </Button>
+                    <Link
+                      href={"/"}
+                      className="text-base font-semibold hover:underline hover:text-blue-400 duration-300"
+                    >
+                      Continue Shopping
+                    </Link>
+                  </div>
+                </div>
               </div>
-              </div>
-              <div className='space-y-4'>
-              <div className='flex items-center justify-between'>
-                <span className='text-lg font-semibold'>Discount</span>
-                <PriceFormatter amount={getSubTotalPrice() - getTotalPrice()}/>
-              </div>
-              </div>
-              <div className='w-full border border-gray-300 mt-1'/>
-              <div className='space-y-4 mt-2'>
-              <div className='flex items-center justify-between'>
-                <span className='text-lg font-semibold'>Total</span>
-                <PriceFormatter amount={getTotalPrice()}/>
-              </div>
-              </div>
-              <div className='space-y-4 py-5 flex flex-col items-center'>
-              <Button onClick={handleCheckOut} className='w-full cursor-pointer' disabled={isLoading}>
-                  {isLoading ? <div className='flex items-center justify-center gap-x-5 w-full text-base'>
-                    <p>Processing...</p>
-                    <span className='text-xl animate-spin'><AiOutlineLoading3Quarters/></span>
-                  </div> :"Proceed to checkout"}
-                </Button>
-                <Link href={'/'} className='text-base font-semibold hover:underline hover:text-blue-400 duration-300'>Continue Shopping</Link>
-              </div>
-          </div>
-        </div>
-          </>
-          ):(
-            <EmptyCart/>
+            </>
+          ) : (
+            <EmptyCart />
           )}
         </Container>
-     ):(
-        <NoAccessToCart/>
-     )}
+      ) : (
+        <NoAccessToCart />
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default CartPage
+export default CartPage;
--- components/PriceQuantity.tsx
+++ components/PriceQuantity.tsx
@@ -1,35 +1,50 @@
-import React from 'react'
-import { Button } from './ui/button'
-import {HiMinus, HiPlus} from "react-icons/hi"
-import toast from 'react-hot-toast'
-import { Product } from '@/sanity.types'
-import userCartStore from '@/store'
-import { cn } from '@/lib/utils'
-interface Props{
-    product:Product;
-    className?:string;
-    bodyStyle?:string
+import React from "react";
+import { Button } from "./ui/button";
+import { HiMinus, HiPlus } from "react-icons/hi";
+import toast from "react-hot-toast";
+import { Product } from "@/sanity.types";
+import userCartStore from "@/store";
+import { cn } from "@/lib/utils";
+interface Props {
+  product: Product;
+  className?: string;
+  bodyStyle?: string;
 }
 
-const PriceQuantity = ({product,className,bodyStyle}:Props) => {
-    const {addItem,removeItem,getItemCount}=userCartStore()
-    const itemCount=getItemCount(product?._id)
-    const handleProductRemove=()=>{
-        removeItem(product?._id)
-        toast.success("product remove")
-    }
-    const handleAddProduct=()=>{
-        addItem(product)
-        toast.success("product added")
-    }
+const PriceQuantity = ({ product, className }: Props) => {
+  const { addItem, removeItem, getItemCount } = userCartStore();
+  const itemCount = getItemCount(product?._id);
+  const handleProductRemove = () => {
+    removeItem(product?._id);
+    toast.success("product remove");
+  };
+  const handleAddProduct = () => {
+    addItem(product);
+    toast.success("product added");
+  };
   return (
-    <div className={cn("flex items-center gap-2",className)}>
-        <Button onClick={handleProductRemove} className='w-6 h-6 text-center cursor-pointer' variant="outline" size="icon"><HiMinus/></Button>
-        <span className='w-8 text-center text-blue-400 text-base'>{itemCount}</span>
-        <Button onClick={handleAddProduct} className='w-6 h-6 text-center cursor-pointer' variant="outline" size="icon"><HiPlus/></Button>
-
+    <div className={cn("flex items-center gap-2", className)}>
+      <Button
+        onClick={handleProductRemove}
+        className="w-6 h-6 text-center cursor-pointer"
+        variant="outline"
+        size="icon"
+      >
+        <HiMinus />
+      </Button>
+      <span className="w-8 text-center text-blue-400 text-base">
+        {itemCount}
+      </span>
+      <Button
+        onClick={handleAddProduct}
+        className="w-6 h-6 text-center cursor-pointer"
+        variant="outline"
+        size="icon"
+      >
+        <HiPlus />
+      </Button>
     </div>
-  )
-}
+  );
+};
 
-export default PriceQuantity
+export default PriceQuantity;
